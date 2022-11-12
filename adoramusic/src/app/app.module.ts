18de96789d54eaf707bb1fc970ba6eeb91bc3cce
Wilmar Galvis <wilmar.galvis@mvm.com.co>
2022-11-12 17:33:27 -0500

    Aprender a crear componentes

--- adoramusic/src/app/app.module.ts
+++ adoramusic/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './view/nav/nav.component';
+import { ContentComponent } from './view/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule
